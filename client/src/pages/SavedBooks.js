5d9b346648bb1dd5b8104770de45ade1a53c25ad
Mackenzie Gray <mdkulen@gmail.com>
2022-10-07 16:38:37 +1100

    modified src/pages/SearchBooks.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React  from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
@@ -11,7 +10,7 @@ import { GET_ME} from '../utils/queries';
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
-  const [ removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const [removeBook] = useMutation(REMOVE_BOOK);
 
   const userData = data?.me || {};
 
@@ -38,7 +37,7 @@ const SavedBooks = () => {
   }; 
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
