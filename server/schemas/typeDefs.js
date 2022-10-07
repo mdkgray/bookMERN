835792e3bec9d6227c962dfb50636387de37ac2f
Mackenzie Gray <mdkulen@gmail.com>
2022-10-07 18:23:05 +1100

    modified saveBook mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -30,7 +30,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth 
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(input: savedBook!): User
+        saveBook(bookData: BookInput!): User
         removeBook(bookId: ID!): User
     }
     type Auth {
