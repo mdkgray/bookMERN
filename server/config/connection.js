e46008d6e580acbccec09b9e1b6682c5234e9d7a
Mackenzie Gray <mdkulen@gmail.com>
2022-10-11 18:10:05 +1100

    deploy to Heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: false,
   useCreateIndex: true,
   useFindAndModify: false,
 });
